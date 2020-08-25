9453300bd911949ec48674b1e6a2445a306cf68b
Mihail Miluk <mihail.miluk@farmin.by>
2020-08-25 11:47:08 +0300

    29 video

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Http\Requests\BlogCategoryCreateRequest;
 use App\Http\Requests\BlogCategoryUpdateRequest;
 use App\Models\BlogCategory;
 use Illuminate\Http\Request;
@@ -27,24 +28,47 @@ class CategoryController extends BaseController
      */
     public function create()
     {
-        dd(__METHOD__);
+        $item = new BlogCategory();
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit',
+            compact('item', 'categoryList'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request)
     {
-        dd(__METHOD__);
+        $data = $request->input();
+        if (empty($data['slug'])) {
+            $data['slug'] = str_slug($data['title']);
+        }
+
+        //Создаст объект но не добавит в бд
+        //$item = new BlogCategory($data);
+        //$item->save();
+
+        //Создаст объект и добавит в БД
+        $item = (new BlogCategory())->create($data);
+
+        if ($item) {
+            return redirect()->route('blog.admin.categories.edit', [$item->id])
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInput();
+        }
+
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -53,29 +77,18 @@ class CategoryController extends BaseController
         $categoryList = BlogCategory::all();
 
         return view('blog.admin.categories.edit',
-        compact ('item', 'categoryList'));
+            compact('item', 'categoryList'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(BlogCategoryUpdateRequest $request, $id)
     {
-         /* $rules = [
-            'title' => 'required|min:5|max:200',
-            'slug' => 'max:200',
-            'description' => 'string|max:500|min:3',
-            'parent_id' => 'required|integer|exists:blog_categories,id',
-        ]; */
-
-        /* $validatedData = $this->validate($request, $rules);
-
-        dd($validatedData); */
-
         $item = BlogCategory::find($id);
         if (empty($item)) {
             return back()
@@ -83,17 +96,21 @@ class CategoryController extends BaseController
                 ->withInput();
         }
 
-            $data = $request->all();
-            $result = $item->fill($data)->save();
+        $data = $request->all();
+        if (empty($data['slug'])) {
+            $data['slug'] = str_slug($data['title']);
+        }
 
-            if ($result) {
-                return redirect()
-                    ->route('blog.admin.categories.edit', $item->id)
-                    ->with(['success' => 'Успешно сохранено']);
-            } else {
-                return back()
-                    ->withErrors(['msg' => 'Ошибка сохранения'])
-                    ->withInpute();
-            }
+        $result = $item->update($data);
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.categories.edit', $item->id)
+                ->with(['success' => 'Успешно сохранено']);
+        } else {
+            return back()
+                ->withErrors(['msg' => 'Ошибка сохранения'])
+                ->withInpute();
+        }
     }
 }
