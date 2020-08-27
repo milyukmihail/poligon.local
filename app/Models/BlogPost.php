9bf6b88b6960e7ea215d6af98a52a8ad814b3deb
Mihail Miluk <mihail.miluk@farmin.by>
2020-08-27 15:09:43 +0300

    Relationship

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -9,4 +9,23 @@ class BlogPost extends Model
 {
     use SoftDeletes;
 
+    /**
+     * Категория статьи
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        // Статья принадлежит категории
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    /**
+     * Автор статьи
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        // Статья принадлежит пользователю
+        return $this->belongsTo(User::class);
+    }
 }
